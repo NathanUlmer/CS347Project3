9cb7f9c9e8b30845de52aa980cfa37c5be1bdfe4
Mattgible <Mattgible@gmail.com>
2021-04-20 13:47:56 -0500

    Cars and Portals and Map and Confusion

---
+++ Real/Project3_real_render/Assets/Scripts/Vehicle/VehicleCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleCamera : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private float transSpeed;
+    [SerializeField] private float rotSpeed;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        HandleTrans();
+        HandleRot();
+    }
+    private void HandleTrans()
+    {
+        var targetPos = target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, targetPos, transSpeed * Time.deltaTime);
+    }
+    private void HandleRot()
+    {
+        var direction = target.position - transform.position;
+        var rot = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, rotSpeed * Time.deltaTime);
+    }
+}
