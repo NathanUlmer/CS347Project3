2b8838a7bf7eb87a30d4ff6aaf8ad008f8e49e7c
NathanUlmer <44823422+NathanUlmer@users.noreply.github.com>
2021-04-21 21:38:44 -0500

    Test for Infected

---
+++ Real/Project3_real_render/Assets/LobbyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class LobbyScript : NetworkBehaviour
+{
+    NetworkLobbyManager lm;
+    NetworkLobbyPlayer lp; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        lm = GetComponent<NetworkLobbyManager>();
+        lp = lm.lobbyPlayerPrefab;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lm.lobbySlots.Length >=4)
+        {
+            
+        }
+    }
+}
---
+++ Real/Project3_real_render/Assets/Scripts/Networking/ServerManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+public class ServerManager : NetworkBehaviour
+{
+    [SyncVar]
+    public float infectedPlayerId;
+
+    NetworkLobbyManager lm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isServer) return;
+        lm = GameObject.Find("NetworkManager").GetComponent<NetworkLobbyManager>();
+        infectedPlayerId = -1;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isServer) return;
+        if(numPlayersInLobby() >= 2 && infectedPlayerId == -1)
+        {
+            RpcChooseInfected();
+        }
+    }
+
+    [ClientRpc]
+    void RpcChooseInfected()
+    {
+        infectedPlayerId = Mathf.RoundToInt(UnityEngine.Random.value * lm.lobbySlots.Length);
+        Debug.Log("Infected Player: " + infectedPlayerId.ToString());
+    }
+
+
+    int numPlayersInLobby()
+    {
+        int count = 0;
+        for(int i = 0; i < lm.lobbySlots.Length; i++)
+        {
+            if (lm.lobbySlots[i] != null) count++;
+        }
+        return count;
+    }
+}
