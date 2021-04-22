b79a425ea9517d88de86431668209083f83f1742
Mattgible <Mattgible@gmail.com>
2021-04-21 23:51:34 -0500

    Ragdoll

---
+++ Real/Project3_real_render/Assets/Scripts/Player/Ragdoll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        setRigidbodyState(true);
+        setColliderState(false);
+        GetComponent<SwordsmanAnimation>().enabled = true;
+        GetComponent<PlayerObjectController>().enabled = true;
+    }
+
+    public void die()
+    {
+        GetComponent<SwordsmanAnimation>().enabled = false;
+        GetComponent<PlayerObjectController>().enabled = false;
+        setRigidbodyState(false);
+        setColliderState(true);
+        if (gameObject != null)
+        {
+            Destroy(gameObject, 3f);
+        }
+    }
+
+    void setRigidbodyState(bool state)
+    {
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = state;
+        }
+        GetComponent<Rigidbody>().isKinematic = !state;
+    }
+
+    void setColliderState(bool state)
+    {
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = state;
+        }
+        GetComponent<Collider>().enabled = !state;
+    }
+}
